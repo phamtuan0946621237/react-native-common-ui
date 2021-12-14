d343c9a33bf7a349027dfab9fe5a453a86a393bd
phamtuan0946621237 <phamtuannd200997@gmail.com>
2021-12-14 18:17:29 +0700

    #update

--- src/screen/qr/qr/index.tsx
+++ src/screen/qr/qr/index.tsx
@@ -1,10 +1,25 @@
-import React, { memo } from "react"
-import { View, Text } from "react-native"
-
-export default memo(() => {
+import { useNavigation } from "@react-navigation/native"
+import React from "react"
+import { Text, View } from "react-native"
+import { Header } from '../../../components/core'
+import { createStyles } from './style'
+export default () => {
+    const navigation : any= useNavigation()
+    const style = createStyles()
+    
     return (
-        <View>
-            <Text>QR_detail</Text>
+        <View style={{ flex: 1 }}>
+            <Header
+                renderTitle={() => {
+                    return <Text style={{ fontWeight: 'bold', lineHeight: 20, textAlign: 'center', fontSize: 16 }}>{"QR"}</Text>
+                }}
+                style={style.header}
+            />
+
+                <View style={{flex : 1,justifyContent : 'center',alignItems : 'center'}}>
+                    <Text onPress={()=> navigation.navigate("QRDetail")}>Go to Detail</Text>
+                </View>
+            
         </View>
     )
-})
+}
--- src/screen/qr/qr_detail/index.tsx
+++ src/screen/qr/qr_detail/index.tsx
@@ -1,10 +1,31 @@
-import React, { memo } from "react"
-import { View, Text } from "react-native"
-
-export default memo(() => {
+import { useNavigation } from "@react-navigation/native"
+import React from "react"
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native"
+import { back_black_ic } from '../../../assets'
+import { Header } from '../../../components/core'
+import { createStyles } from './style'
+export default () => {
+    const navigation : any= useNavigation()
+    const style = createStyles()
+    
     return (
-        <View>
-            <Text>QR</Text>
+        <View style={{ flex: 1 }}>
+            <Header
+                renderLeft={() => {
+                    return (
+                        <TouchableOpacity style={{ padding: 16 }} onPress={navigation.goBack}>
+                            <Image source={back_black_ic} style={style.icon24} />
+                        </TouchableOpacity>
+                    );
+                }}
+                renderTitle={() => {
+                    return <Text style={{ fontWeight: 'bold', lineHeight: 20, textAlign: 'center', fontSize: 16 }}>{"Nhập số tiền thanh toán"}</Text>
+                }}
+                style={style.header}
+            />
+            <ScrollView>
+            
+            </ScrollView>
         </View>
     )
-})
+}
