3d1871194a9a0e7b2d0437550eb9c780accfdda0
phamtuan0946621237 <phamtuannd200997@gmail.com>
2021-12-14 14:37:21 +0700

    #update

--- src/components/naivgation.tsx
+++ src/components/naivgation.tsx
@@ -1,11 +1,23 @@
 import React, { memo } from 'react'
-import { Text } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
+import { Text,View } from 'react-native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function ButtonView () {
+function Screen1 () {
+    const navigation : any= useNavigation()
     return (
-        <Text>ButtonView</Text>
+        <View style={{flex : 1,justifyContent : 'center',alignItems : 'center'}}>
+        <Text onPress={() => navigation.navigate("Screen2")}>Go to Screen2</Text>
+        </View>
+    )
+}
+
+function Screen2 () {
+    const navigation : any= useNavigation()
+    return (
+        <View style={{flex : 1,justifyContent : 'center',alignItems : 'center'}}>
+        <Text onPress={() => navigation.navigate("Screen1")}>go to screen1</Text>
+        </View>
     )
 }
 const Stack = createStackNavigator<any>();
@@ -17,10 +29,10 @@ export default memo(() => {
                     headerShown: false,
                 }}
                 initialRouteName={
-                    "ButtonView"
+                    "Screen1"
                 }>
-                <Stack.Screen name="ButtonView" component={ButtonView} options={{ title: 'Intro' }} />
-                
+                <Stack.Screen name="Screen1" component={Screen1} options={{ title: 'Intro' }} />
+                <Stack.Screen name="Screen2" component={Screen2} options={{ title: 'Intro' }} />
             </Stack.Navigator>
         </NavigationContainer>
     )
