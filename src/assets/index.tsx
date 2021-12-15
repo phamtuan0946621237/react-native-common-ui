55ec36aab1e706e132fd09761f887d6070f2578a
phamtuan0946621237 <phamtuannd200997@gmail.com>
2021-12-15 11:29:09 +0700

    #update

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,1 +1,2 @@
 export const back_black_ic = require('./imgs/back_black_ic.png')
+export const qr = require('./imgs/qr.png')
--- src/screen/qr/qr/index.tsx
+++ src/screen/qr/qr/index.tsx
@@ -1,12 +1,16 @@
 import { useNavigation } from "@react-navigation/native"
-import React from "react"
-import { Text, View } from "react-native"
+import React, { useState, useCallback } from "react"
+import { Text, View ,TouchableOpacity,Image} from "react-native"
 import { Header } from '../../../components/core'
 import { createStyles } from './style'
+import {qr} from '../../../assets'
 export default () => {
-    const navigation : any= useNavigation()
+    const navigation: any = useNavigation()
     const style = createStyles()
-    
+    const [indexTab, setIndexTab] = useState<number>(0)
+    const _selectTab = useCallback((index: number) => {
+        setIndexTab(index)
+    }, [])
     return (
         <View style={{ flex: 1 }}>
             <Header
@@ -16,10 +20,23 @@ export default () => {
                 style={style.header}
             />
 
-                <View style={{flex : 1,justifyContent : 'center',alignItems : 'center'}}>
-                    <Text onPress={()=> navigation.navigate("QRDetail")}>Go to Detail</Text>
+            <View style={[{ backgroundColor: 'white', paddingVertical: 16, paddingHorizontal: 12, flexDirection: 'row', alignItems: 'center' }, style.header]}>
+                {["Mã của tôi", "Quét mã"].map((item: string, index: number) => {
+                    return (
+                        <TouchableOpacity onPress={() => _selectTab(index)} key={index} style={{ flex: 1, height: 42, backgroundColor: indexTab === index ? '#8E94F2' : 'white', marginHorizontal: 4, justifyContent: 'center', alignItems: 'center', borderRadius: 12, borderWidth: indexTab !== index ? 1 : 0, borderColor: '#8E94F2' }}>
+                            <Text style={{ fontSize: 16, color: index === indexTab ? 'white' : '#8E94F2' }}>{item}</Text>
+                        </TouchableOpacity>
+                    )
+                })}
+            </View>
+
+            {indexTab === 0 ?
+                <View style={{ flex: 1 }}>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 24, textAlign: 'center' }}>Dùng mã này để thanh toán</Text>
+                    <Image source={qr} style={{ alignSelf: 'center', marginTop: 24 }} />
                 </View>
-            
+                : <></>}
+
         </View>
     )
 }
