f8bd52a415834809d6eb90178e8ac52f6effdf74
phamtuan0946621237 <phamtuannd200997@gmail.com>
2021-12-15 15:54:01 +0700

    #update

--- src/screen/qr/qr_detail/index.tsx
+++ src/screen/qr/qr_detail/index.tsx
@@ -5,9 +5,9 @@ import { back_black_ic } from '../../../assets'
 import { Header } from '../../../components/core'
 import { createStyles } from './style'
 export default () => {
-    const navigation : any= useNavigation()
+    const navigation: any = useNavigation()
     const style = createStyles()
-    
+
     return (
         <View style={{ flex: 1 }}>
             <Header
@@ -24,7 +24,12 @@ export default () => {
                 style={style.header}
             />
             <ScrollView>
-            
+                <View style={[{ alignItems: 'center', padding: 16, marginTop: 16 }, style.header]}>
+                    <Text style={{ color: 'rgba(74, 74, 74, 1)', fontSize: 16, fontWeight: '400' }}>Ngừoi nhận</Text>
+                    <Image source={{ uri: "https://assets-global.website-files.com/5db63dc519de55e028ef4816/5e786f450014e62b221e31d3_Eimi%20Fukada%20-%201.jpg" }} style={{ width: 64, height: 64, borderRadius: 32, marginTop: 16, marginBottom: 12 }} />
+                    <Text style={{ fontSize: 24, lineHeight: 28, fontWeight: 'bold' }}>7 eleven</Text>
+                    <Text style={{ fontSize: 16, fontWeight: '400', marginTop: 5 }}>7 eleven - Trần Duy Hưng</Text>
+                </View>
             </ScrollView>
         </View>
     )
